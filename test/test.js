dc60566f3b7e1d4960b12be2e42b8fc4cdcd962b
Aleksandr Panev <aleksandr.panev.2002@gmail.com>
2022-10-02 14:04:18 +0000

    add first real test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,24 @@
-var assert = require('assert');
+var expect = require('chai').expect
+const cds = require ('@sap/cds')
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+      expect([1, 2, 3].indexOf(4)).to.equal(-1)
+    })
+  })
+})
+
+describe('Interaction service handler', function () {
+
+    const { GET, axios } = cds.test()
+    axios.defaults.auth = { username: 'any', password: 'any' }
+
+    describe('Items', function() {
+        it('should return data', async function() {
+            const {data, status} = await GET`/catalog/Interactions_Header/1`
+            expect(status).to.eql(200)
+            expect(data).to.contain({"ID":1})
+        })
+    })
+})
