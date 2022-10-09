6d099cc0b3836b5c67e89e34d58d3a3248cd28db
Oriery <panev291002@gmail.com>
2022-10-09 20:42:28 +0300

    fix typo

--- mocks/server.js
+++ mocks/server.js
@@ -3,7 +3,7 @@ const { rest } = require('msw')
 
 module.exports = setupServer(
     rest.get('https://catfact.ninja/fact', (req, res, ctx) => {
-        catFact = { 'fact': 'testCatFact', 'length': 8 }
+        catFact = { 'fact': 'testCatFact', 'length': 11 }
 
         return res(ctx.json(catFact))
     })
