26a42c3d69328f1a1b6503fa90e26d7fb94313c4
Aleksandr Panev <aleksandr.panev.2002@gmail.com>
2022-10-18 18:26:52 +0000

    minor

--- test/test.js
+++ test/test.js
@@ -7,8 +7,6 @@ const impl = require('../srv/interaction_srv.js')
 const implNorthWind = require('../srv/catalog-service.js')
 const serverMSW = require('../mocks/server.js')
 
-module.exports = { cds }
-
 before('MSW: Establish API mocking before all tests.', () => serverMSW.listen({ onUnhandledRequest: 'bypass' }))
 afterEach('MSW: Reset any request handlers that we may add during the tests', () => serverMSW.resetHandlers())
 after('MSW: Clean up after the tests are finished.', () => serverMSW.close())
@@ -25,9 +23,7 @@ describe('CDS services', function () {
     const { GET, axios } = cds.test()
     axios.defaults.auth = { username: 'any', password: 'any' }
 
-    afterEach(function () {
-        chai.spy.restore()
-    })
+    afterEach(() => chai.spy.restore())
 
     describe('CatalogService', function () {
         it('should return data', async function () {
@@ -37,10 +33,6 @@ describe('CDS services', function () {
         })
 
         describe('handler of service', function () {
-            afterEach(function () {
-                chai.spy.restore()
-            })
-
             describe('modifyLOGTEXT()', function () {
                 it('should change data properly', async function () {
                     const data = [{
@@ -74,18 +66,12 @@ describe('CDS services', function () {
             var cds_connect_to = implNorthWind.cds.connect.to
             chai.spy.on(implNorthWind.cds.connect, 'to', async function (nameOfModule) {
                 if (nameOfModule === 'NorthWind') {
-                    console.log("##################################################### mocking cds.connect.to")
                     return require('../mocks/NorthWind_mock.js')
                 } else {
-                    console.log("##################################################### calling original cds.connect.to")
                     return await cds_connect_to(nameOfModule)
                 }
             })
         })()
-
-        after(function () {
-            chai.spy.restore()
-        })
 
         it('should return data', async function () {
             // expect(implNorthWind.cds.connect.to).to.be.spy; 
